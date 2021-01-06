fc9bed9dfd3b8c54052d326039aa4b89dd1ded18
Omas Ajiri <jicintech@gmail.com>
2021-01-06 10:12:51 +0100

    fix: update seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Layout from '../components/layout'
 import Hero from '../components/hero'
 import Home from '../components/home'
+import SEO from "../components/seo"
+
 const IndexPage = ({ data }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
     return (
         <Layout location='/' title={siteTitle}>
+           <SEO title="Omas Ajiri" />
             <Hero />
             <Home />
         </Layout>
