4dd1967ae3d7c0bc6e5add9ffcc2a7025ce71ec6
Omas Ajiri <jicintech@gmail.com>
2021-01-10 23:36:58 +0100

    feat: added a send btn on the contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,6 +9,8 @@ const Contact = () => {
             <div className="about-wrap">
             
                     <p>I am developer and a writer | Pharmacist</p>
+            <a href="mailto:omasajiri@gmail.com" className='btn' style={{color: 'blue'}}>Send a Message</a>
+
             
             </div>
         </div>
