4c9688ccef3c733aae4c8b8bf701787324abb565
Omas Ajiri <jicintech@gmail.com>
2021-01-11 21:09:35 +0100

    fix: updated the send btn on the contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
             <div className="about-wrap">
             
                     <p>I am developer and a writer | Pharmacist</p>
-            <a href="mailto:omasajiri@gmail.com" className='btn' style={{color: 'blue'}}>Send a Message</a>
+            <a href="mailto:omasajiri@gmail.com" className='btn contact-btn'>Send a Message</a>
 
             
             </div>
