6d3530311cbf0e001a820c783116d2657b4235b5
Omas Ajiri <jicintech@gmail.com>
2021-04-07 20:24:35 +0100

    fix: config

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,13 +61,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Omas Ajiri`,
+        short_name: `OmasAjiri`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#005b99`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/omas-ajiri.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
