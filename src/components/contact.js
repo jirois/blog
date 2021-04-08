de4eabb374ade65e4660251fff714379b01fe095
Omas Ajiri <jicintech@gmail.com>
2021-04-08 16:18:15 +0100

    fix: contactpage fixes

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,25 +11,25 @@ const Contact = () => {
             <div className="contact-wrap">
                 <div className="contact-message">
             
-                    <p>Hi!, How about we connect today! You can me react ASAP by sending a message </p>
+                    <p>Hi!, How about we connect today! You can reach me ASAP by sending a message </p>
                 <a href="mailto:omasajiri@gmail.com" className='btn contact-btn'>Send a Message</a>
                 </div>
                 <div className="contact-or">Or</div>
                 <div className="contact-message">
                     <div className="social-media">
-                    <p>Through my social media handles</p>
+                    <span className="social-text">Through my social media handles</span>
                         <div className="contact-media">
-                        <a href="/facebook" className="social" target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faFacebook} className="icons"/>
+                        <a href="/facebook" className="social " target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faFacebook} className="icons facebook-icon"/>
                             <span>facebook</span> 
                         </a>
-                        <a href="https://twitter.com/ajiriomas" className="social" target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faTwitter} className="icons"/>
+                        <a href="https://twitter.com/ajiriomas" className="social " target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faTwitter} className="icons twitter-icon"/>
 
                            <span>twitter</span> 
                         </a>
-                        <a href="https://www.linkledin.com" className="social" target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faLinkedin} className="icons"/>
+                        <a href="https://www.linkledin.com" className="social " target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} className="icons linkledin-icon"/>
 
                            <span>linkedlin</span> 
                         </a>
