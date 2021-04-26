1b4fb0c598c40b87a0383facf05a2909d7be4a98
Omas Ajiri <jicintech@gmail.com>
2021-04-26 10:35:39 +0100

    fix: updated layout page

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import './hero.scss'
+import React from "react"
+import "./hero.scss"
 const Hero = () => {
-    return (
-        <>
-        <div className="html">
-        <div className='stars'></div>
-        <div className='stars2'></div>
-        <div className='stars3'></div>
-        </div>
-        </>
-    )
+  return (
+    <div className="html">
+      <div className="stars"></div>
+      <div className="stars2"></div>
+      <div className="stars3"></div>
+    </div>
+  )
 }
-export default Hero;
+export default Hero
