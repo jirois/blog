4d2b7651bfab580aa9c1af45d417c6ecf2d4d62f
Omas Ajiri <jicintech@gmail.com>
2021-04-26 15:41:36 +0100

    fix: updated contact page

--- src/common/aboutCarousel.js
+++ src/common/aboutCarousel.js
@@ -1,44 +1,49 @@
-import React, { useState, useEffect} from 'react'
-import { useTransition, animated, config } from 'react-spring'
-import './aboutSlider.css';
+import React, { useState, useEffect } from "react"
+import { useTransition, animated, config } from "react-spring"
+import "./aboutSlider.css"
 
-const slides = [
+export const slides = [
   {
     id: 1,
-    url: 'https://res.cloudinary.com/jinncy/image/upload/v1613725628/mblog/omas_2_drvgve.jpg',
-    text: 'Am a developer'
-},
-{
+    url:
+      "https://res.cloudinary.com/jinncy/image/upload/v1613725628/mblog/omas_2_drvgve.jpg",
+    text: "Am a developer",
+  },
+  {
     id: 2,
-    url: 'https://res.cloudinary.com/jinncy/image/upload/v1618306772/mblog/omas_4_hqkkw4.png',
-    text: 'Am a writer'
-},
-{
+    url:
+      "https://res.cloudinary.com/jinncy/image/upload/v1618306772/mblog/omas_4_hqkkw4.png",
+    text: "Am a writer",
+  },
+  {
     id: 3,
-    url: 'https://res.cloudinary.com/jinncy/image/upload/v1613725530/mblog/omas_3_qhe0j5.jpg',
-    text: 'Am a Pharmacist'
-},
-  
+    url:
+      "https://res.cloudinary.com/jinncy/image/upload/v1613725530/mblog/omas_3_qhe0j5.jpg",
+    text: "Am a Pharmacist",
+  },
 ]
 
-
-const Carousel = () =>  {
-
-  const [index, set] = useState(0);
+const Carousel = () => {
+  const [index, set] = useState(0)
   const transitions = useTransition(slides[index], item => item.id, {
-    from: {opacity: 0},
-    enter: { opacity: 1},
-    leave: { opacity: 0},
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
     config: config.molasses,
   })
 
-  useEffect(() => void setInterval(() => set(state => (state + 1) % 3), 4000), [])
+  useEffect(
+    () => void setInterval(() => set(state => (state + 1) % 3), 4000),
+    []
+  )
   return transitions.map(({ item, props, key }) => (
-    
-    <animated.div key={key}
-    className="bg"
-    style={{...props, backgroundImage: `url(${item.url})`}} ><span className="about-text">{item.text}</span></animated.div>
-   
+    <animated.div
+      key={key}
+      className="bg"
+      style={{ ...props, backgroundImage: `url(${item.url})` }}
+    >
+      <span className="about-text">{item.text}</span>
+    </animated.div>
   ))
 }
 export default Carousel
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,45 +1,81 @@
-import React from 'react';
-import './contact.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitter, faLinkedin} from "@fortawesome/free-brands-svg-icons"
+import React from "react"
+import "./contact.scss"
+import { slides } from "../common/aboutCarousel"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons"
 
 const Contact = () => {
-    return (
-        <div className="contact">
-            <h1>Contact</h1>
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
 
-            <div className="contact-wrap">
-                <div className="contact-message">
-            
-                    <p>Hi!, How about we connect today! You can reach me ASAP by sending a message </p>
-                <a href="mailto:omasajiri@gmail.com" className='btn contact-btn'>Send a Message</a>
-                </div>
-                <div className="contact-or">Or</div>
-                <div className="contact-message">
-                    <div className="social-media">
-                    <span className="social-text">Through my social media handles</span>
-                        <div className="contact-media">
-                        <a href="/facebook" className="social " target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faFacebook} className="icons facebook-icon"/>
-                            <span>facebook</span> 
-                        </a>
-                        <a href="https://twitter.com/ajiriomas" className="social " target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faTwitter} className="icons twitter-icon"/>
-
-                           <span>twitter</span> 
-                        </a>
-                        <a href="https://www.linkledin.com" className="social " target="_blank" rel="noreferrer">
-                        <FontAwesomeIcon icon={faLinkedin} className="icons linkledin-icon"/>
-
-                           <span>linkedlin</span> 
-                        </a>
-                        
-                        </div>
-                    </div>
-                </div>
-            
-            </div>
+      <div className="contact-wrap">
+        <div className="message-box">
+          <div className="contact-img">
+            <img src={slides[1].url} alt={slides[1].text} />
+          </div>
+          <div className="contact-message">
+            <p>
+              Hi, Let's connect! You can reach out to me ASAP by sending a
+              message{" "}
+            </p>
+            <a href="mailto:omasajiri@gmail.com" className="btn contact-btn">
+              Send a Message
+            </a>
+          </div>
         </div>
-    )
+        <div className="contact-or">Or</div>
+        <div className="contact-message">
+          <div className="social-media">
+            <span className="social-text">Through my social media handles</span>
+            <div className="contact-media">
+              <a
+                href="/facebook"
+                className="social "
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  className="icons facebook-icon"
+                />
+                <span>facebook</span>
+              </a>
+              <a
+                href="https://twitter.com/AjiriOmas"
+                className="social "
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="icons twitter-icon"
+                />
+
+                <span>twitter</span>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/omasajiri/"
+                className="social "
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className="icons linkledin-icon"
+                />
+
+                <span>linkedlin</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
-export default Contact;
+export default Contact
