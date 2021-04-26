f3372003bfd5c2945b28365d9fbd4ab6e58546bb
Omas Ajiri <jicintech@gmail.com>
2021-04-26 10:41:17 +0100

    fix: updated layout page and home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import Image from "gatsby-image"
-import './home.scss'
-import {WordsFading} from '../common/wordsFadding'
+import "./home.scss"
+// import { WordsFading } from "../common/wordsFadding"
 const Home = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query HomeQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
@@ -36,59 +36,54 @@ const Home = () => {
 
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
-//   const social = data.site.siteMetadata?.social
+  //   const social = data.site.siteMetadata?.social
 
   const avatar = data?.avatar?.childImageSharp?.fixed
 
   const logo = data?.logo?.childImageSharp?.fixed
-  
 
-
-
-  
-  
-    return (
-        <div className='home'>
-           <div className='logo-container'>
-          {logo && 
+  return (
+    <div className="home">
+      <div className="logo-container">
+        {logo && (
           <Image
             fixed={logo}
             alt={author?.name || ``}
             className="home-logo"
             imgStyle={{
-                borderRadius: `50%`,
-               
-            }} 
+              borderRadius: `50%`,
+            }}
           />
-          }
-          </div>
-            <div className='avatar-container'>
-          {avatar && 
+        )}
+      </div>
+      <div className="avatar-container">
+        {avatar && (
           <Image
             fixed={avatar}
             alt={author?.name || ``}
             className="home-avatar"
             imgStyle={{
-                borderRadius: `50%`,
-               
-            }} 
+              borderRadius: `50%`,
+            }}
           />
-        } 
-        </div>
-        <div>
-        <h1 className="heading-h1">Hi There, <br />
-        <span> I'm {WordsFading}</span> </h1>
-        </div>
-        <div className='btnContainer'>
-                <button className='btn aboutme-btn'>
-                    <Link to="/about">  About Me</Link> 
-                </button>
-            <button className='btn contactme-btn'>
-             <Link to="/contact">Contact Me</Link>
-            </button>
-        </div>
-        </div>
-    )
+        )}
+      </div>
+      <div>
+        <h1 className="heading-h1">
+          Hi There, <br />
+          <span> I'm Omas Ajiri </span>{" "}
+        </h1>
+      </div>
+      <div className="btnContainer">
+        <button className="btn aboutme-btn">
+          <Link to="/about"> About Me</Link>
+        </button>
+        <button className="btn contactme-btn">
+          <Link to="/contact">Contact Me</Link>
+        </button>
+      </div>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
